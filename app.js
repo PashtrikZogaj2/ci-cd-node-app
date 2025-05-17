84af2225ef2cc7a36be28f71224fe04d44bafa2c
Pashtrik Zogaj <pashtrikzogaj03@outlook.com>
2025-05-17 14:50:03 +0200

    Shto testet

--- app.js
+++ app.js
@@ -5,5 +5,53 @@ app.get('/', (req, res) => {
   res.send('Hello from Pashtrik and Lorik');
 });
 
+app.get('/todos', (req, res) => {
+  res.send(`
+    <h1>To-Do List</h1>
+    <ul>
+      <li>Bëj ushtrime</li>
+    </ul>
+  `);
+});
+
+app.get('/users', (req, res) => {
+  res.send(`
+    <h1>User List</h1>
+    <ul>
+      <li>Pashtrik</li>
+    </ul>
+  `);
+});
+
 module.exports = app;
 
+
+
+
+
+
+
+
+
+
+app.get('/todos', (req, res) => {
+  res.send(`
+    <h1>To-Do List</h1>
+    <ul>
+      <li>Bëj ushtrime</li>
+      <li>Lexo për test</li>
+      <li>Testo CI/CD</li>
+    </ul>
+  `);
+});
+
+app.get('/users', (req, res) => {
+  res.send(`
+    <h1>User List</h1>
+    <ul>
+      <li>Pashtrik</li>
+      <li>Lorik</li>
+      <li>TestUser</li>
+    </ul>
+  `);
+});
--- test/app.test.js
+++ test/app.test.js
@@ -1,9 +1,27 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../app'); // sigurohu që ky rrugë është e saktë
 
 describe('GET /', () => {
   it('kthe tekstin korrekt', async () => {
     const res = await request(app).get('/');
     expect(res.text).toBe('Hello from Pashtrik and Lorik');
+  });
+});
+
+describe('GET /todos', () => {
+  it('kthe HTML me listën e detyrave', async () => {
+    const res = await request(app).get('/todos');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toContain('<h1>To-Do List</h1>');
+    expect(res.text).toContain('<li>Bëj ushtrime</li>');
+  });
+});
+
+describe('GET /users', () => {
+  it('kthe HTML me listën e përdoruesve', async () => {
+    const res = await request(app).get('/users');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toContain('<h1>User List</h1>');
+    expect(res.text).toContain('<li>Pashtrik</li>');
   });
 });
