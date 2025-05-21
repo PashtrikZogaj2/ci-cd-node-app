449dfbe3f96bcbd38aeaa973f887e2399199c2ed
Pashtrik Zogaj <pashtrikzogaj03@outlook.com>
2025-05-21 15:29:04 +0200

    HAPI PARE

--- app.js
+++ app.js
@@ -1,32 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json());
-
-let todos = [
-  { id: 1, title: 'Ushtrime' },
-  { id: 2, title: 'Test CI/CD' }
-];
-
 app.get('/', (req, res) => {
   res.send('Hello from Pashtrik and Lorik');
-});
-
-app.get('/todos', (req, res) => {
-  const items = todos.map(todo => `<li>${todo.title}</li>`).join('');
-  const html = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>To-Do List</title>
-      </head>
-      <body>
-        <h1>To-Do List</h1>
-        <ul>${items}</ul>
-      </body>
-    </html>
-  `;
-  res.send(html);
 });
 
 module.exports = app;
